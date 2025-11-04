9c58cce7c4b66daff4bd31a101c7759ecc791dff
Administrator <Administrator@WIN-77H43AOMK2B>
2025-11-04 18:16:10 +0800

    ...

---
+++ backend/PharmacyApp.Infrastructure/Migrations/20251031140829_ResetIdentitySeed.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace PharmacyApp.Infrastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class ResetIdentitySeed : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
